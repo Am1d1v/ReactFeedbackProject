d7f9ab022755b77a35b2546596573d557d05351c
Am1d1v <dimadiman1adima@gmail.com>
2024-01-12 17:12:03 +0300

    isNaN issue fixed

--- src/components/FeedbackStats/FeedbackStats.jsx
+++ src/components/FeedbackStats/FeedbackStats.jsx
@@ -10,7 +10,7 @@ function FeedbackStats({feedback}) {
   return (
     <div className='feedback-stats'>
         <h4>{feedback.length} Reviews</h4>
-        <h4>Average Rating: {average.toFixed(1)}</h4>
+        <h4>Average Rating: {isNaN(average) ? <h4>0</h4> : average.toFixed(1)}</h4>
     </div>
   )
 }
