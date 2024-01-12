a8f421f5c9d2f655455a3534c3c046a96a42e1de
Am1d1v <dimadiman1adima@gmail.com>
2024-01-12 17:08:14 +0300

    Average Rating

--- src/components/FeedbackStats/FeedbackStats.jsx
+++ src/components/FeedbackStats/FeedbackStats.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
 
 function FeedbackStats({feedback}) {
+
+    // Calculate Average Rating
+    let average = feedback.reduce((acc, cur) => {
+       return ((acc += cur.rating));
+    }, 0) / feedback.length;
+
   return (
-    <div className='feedback-stats'><h4>{feedback.length} Reviews</h4></div>
+    <div className='feedback-stats'>
+        <h4>{feedback.length} Reviews</h4>
+        <h4>Average Rating: {average.toFixed(1)}</h4>
+    </div>
   )
 }
 
