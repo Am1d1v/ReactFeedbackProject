ebeefa9b94018cef2390bed145ca0729ae7abc88
Am1d1v <dimadiman1adima@gmail.com>
2024-01-12 17:33:58 +0300

    Feedback Form

--- src/components/FeedbackFrom/FeedbackForm.jsx
+++ src/components/FeedbackFrom/FeedbackForm.jsx
@@ -8,12 +8,16 @@ function FeedbackForm() {
     // Review Text
     const [text, setText] = useState('');
 
+    const handleTextChange = (event) => {
+        setText(event.target.value);
+    }
+
   return (
     <Card>
         <form>
             <h2>How would you rate this app ?</h2>
             <div className="input-group">
-                <input type="text" placeholder='Write a review'/>
+                <input type="text" placeholder='Write a review' onChange={handleTextChange} value={text}/>
                 <button type="submit">Send</button>
             </div>
         </form>
