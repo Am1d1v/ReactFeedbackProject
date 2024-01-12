407fb2492b87a5079350bbff393280ec5f7cd438
Am1d1v <dimadiman1adima@gmail.com>
2024-01-12 15:52:28 +0300

    React Icons

--- src/components/FeedbakcItem/FeedbackItem.jsx
+++ src/components/FeedbakcItem/FeedbackItem.jsx
@@ -1,10 +1,14 @@
+import {FaTimes} from 'react-icons/fa'
 import Card from "../shared/Card"
 
 function FeedbackItem({rating, text}) {
 
   return (
-    <Card reverse={false}>
+    <Card reverse={true}>
         <div className="num-display">{rating}</div>
+        <button className="close">
+          <FaTimes color="purple"/>
+        </button>
         <div className="text-display">{text}</div>
     </Card>
   )
