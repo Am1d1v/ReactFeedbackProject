1911b58da2388da83301128f0e32ee9159ec2d8d
Am1d1v <dimadiman1adima@gmail.com>
2024-01-13 17:04:40 +0300

    Feedback validation

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-function Button({children, version = 'primary', type, isDisabled = false}) {
+function Button({children, version = 'primary', type, isDisabled = true}) {
   return (
     <button type={type} disabled={isDisabled} className={`btn btn-${version}`}>
         {children}
--- src/components/FeedbackFrom/FeedbackForm.jsx
+++ src/components/FeedbackFrom/FeedbackForm.jsx
@@ -8,9 +8,17 @@ function FeedbackForm() {
 
     // Review Text
     const [text, setText] = useState('');
+    const [btnDisabled, setBtnDisabled] = useState(true);
 
     const handleTextChange = (event) => {
         setText(event.target.value);
+
+        // If Feedback text length less than 10 symbols we cannot send feedback message
+        if(text.length < 10){
+            setBtnDisabled(true);
+        } else {
+            setBtnDisabled(false)
+        }
     }
 
   return (
@@ -19,7 +27,7 @@ function FeedbackForm() {
             <h2>How would you rate this app ?</h2>
             <div className="input-group">
                 <input type="text" placeholder='Write a review' onChange={handleTextChange} value={text}/>
-                <Button type="submit" >Send</Button>
+                <Button type="submit" isDisabled={btnDisabled}>Send</Button>
             </div>
         </form>
     </Card>
